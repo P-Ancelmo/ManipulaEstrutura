5708d04ab21ce7f4088247bd761f4725a2e5675a
LucasAC18 <56647034+LucasAC18@users.noreply.github.com>
2019-10-17 21:42:04 -0300

    Update struct2.c

--- struct2.c
+++ struct2.c
@@ -59,3 +59,31 @@ void buscaNome (PESSOA agenda[]) { //busca pessoas pelo nome (c)
 		}
 	}
 }
+
+void buscaMes (PESSOA agenda[]) { //busca pessoas pelo mes de aniversário (d)
+	char mes;
+	int v = 1;
+	printf("insira o mes desejado:\n");
+	gets(mes);
+	while (mes < 1 || mes > 12) {
+		printf("insira um mês válido:\n");
+		gets(mes);
+	}
+	for (int i = 0; i > 100; i++) {
+		if (agenda[i].data.mes == mes) {
+			printf("nome: %s\n", agenda[i].nome);
+			printf("eMail: %s\n", agenda[i].eMail);	
+			printf("rua: %s\n", agenda[i].endereco.rua);	
+			printf("numero: %d\n", agenda[i].endereco.numero);	
+			printf("complemento: %s\n", agenda[i].endereco.complemento);	
+			printf("bairro: %s\n", agenda[i].endereco.bairro);	
+			printf("CEP: %s\n", agenda[i].endereco.cep);	
+			printf("cidade: %s\n", agenda[i].endereco.cidade);	
+			printf("estado: %s\n", agenda[i].endereco.estado);	
+			printf("país: %s\n", agenda[i].endereco.pais);	
+			printf("telefone: %s%s\n", agenda[i].telefone.ddd, agenda[i].telefone.numero);	
+			printf("aniversário: %s/%s/%s\n", agenda[i].data.dia, agenda[i].data.mes, agenda[i].data.ano);	
+			printf("Observações: %s\n", agenda[i].observacao);	
+		}
+	}
+}
